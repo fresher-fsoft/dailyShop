6266345921d9cc9a9059ed407bba4b38053f32b2
Thanh <thanhtt9900@gmail.com>
2019-01-10 15:10:36 +0700

    delete cart item

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../model/product';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  cartProduct: Product[];
+  public listCart;
+  constructor(private cartService: CartService) { }
 
   ngOnInit() {
+    this.listCart = this.getCartProduct();
+    console.log(this.listCart);
   }
 
+  getCartProduct(){
+    return this.cartService.productsCart;
+  }
+  deleteCartItem(i){
+    this.listCart.splice(i, 1);
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import { Product } from '../model/product';
 
 @Injectable({
@@ -7,25 +6,24 @@ import { Product } from '../model/product';
 })
 export class CartService {
   productsCart: Product[];
-  totalProductsCart: number = 0;
-
-  constructor() { 
-    this.productsCart = [
-      new Product(1, 't-shirt', 'men'),
-      new Product(2, 'a-shirt', 'men'),
-      new Product(3, 'b-shirt', 'men'),
-    ]  
-  }
+  totalProductsCart  = 0;
+  productsCart = [
+    new Product(1, 't-shirt', 'men'),
+    new Product(2, 'a-shirt', 'men'),
+    new Product(3, 'b-shirt', 'men'),
+  ];
 
   getTotalProductsCart(): number{
     return this.totalProductsCart = this.productsCart.length;
   }
 
   getProductsCarts(): Product[]{
-    return this.productsCart
+    return this.productsCart;
   }
 
   addToCart(product: Product){
-    this.productsCart.push(product)
+    this.productsCart.push(product);
+  }
+  deleteCart(){
   }
 }
