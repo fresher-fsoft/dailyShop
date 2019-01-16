ebe9aee91cb0b63c48f10d2b1f1b620eb21e4604
nghia <huunghiaphan95@gmail.com>
2019-01-16 16:10:06 +0700

    update layout

--- daily-shop/src/app/components/product/product.component.ts
+++ daily-shop/src/app/components/product/product.component.ts
@@ -27,7 +27,7 @@ export class ProductComponent implements OnInit {
         this.productsMen    = this.products.filter(product => product.type == 'men');
         this.productsWomen  = this.products.filter(product => product.type == 'women');
 
-        this.productsTmp = this.productsMen.slice(0, 8);
+        this.productsTmp = this.productsMen;
       });
     });
   }
@@ -37,10 +37,10 @@ export class ProductComponent implements OnInit {
     
     switch(type){
       case 'men': 
-        this.productsTmp = this.productsMen.slice(0, 8);
+        this.productsTmp = this.productsMen;
         break;
       case 'women': 
-        this.productsTmp = this.productsWomen.slice(0, 8);
+        this.productsTmp = this.productsWomen;
         break;
     }
 
